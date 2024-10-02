c775d0f0406979b8e47f33d55f82f67060967d89
kcommerce <kcommerce@gmail.com>
2024-10-02 17:21:33 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// Display errors (Security Misconfiguration)
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Session without security settings (Session Misconfiguration)
+session_start();
+?>
