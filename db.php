ad734d13b50578ffd4b839df386aafea8754132a
kcommerce <kcommerce@gmail.com>
2024-10-02 17:20:49 +0700

    Create db.php

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+// Sensitive data hardcoded (Sensitive Data Exposure)
+$servername = "localhost";
+$username = "root";
+$password = "root123";  // Hardcoded credentials
+$dbname = "test_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
